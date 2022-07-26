d215382126d965a7e604aa5c712ad0a04be8ce70
love shopify <devshopify207@gmail.com>
2022-07-26 10:02:25 -0700

    update login page

---
+++ assets/map.js
@@ -0,0 +1,129 @@
+var physicians = [[{ lat: 43.7730424, lng: -79.4141723 }, "Rejuuv Medispa"],[{ lat: 43.6723496, lng: -79.3846885 }, "Dermatology on Bloor "],[{ lat: 43.6723496, lng: -79.3846885 }, "Dermatology on Bloor "],[{ lat: 43.5341635, lng: -79.6714012 }, "O&#39;Neill Cosmetic Dermatology"],[{ lat: 43.6860342, lng: -79.3936282 }, "Rosedale Dermatology Centre"],[{ lat: 43.6968386, lng: -79.3961094 }, "Gidon Aesthetics and MediSpa"],[{ lat: 49.185119, lng: -123.129476 }, "Richmond Laser Skin Care &amp; Wellness Centre"],[{ lat: 49.1172699, lng: -122.8907991 }, "Delta Laser &amp; Skincare Centre"],[{ lat: 49.2502266, lng: -122.8958023 }, "Lougheed Laser Centre"],[{ lat: 43.8237873, lng: -79.3268837 }, "ID Cosmetic Clinic"],[{ lat: 49.2638726, lng: -123.1342629 }, "Broadway Cosmetic Plastic Surgery Centre"],[{ lat: 43.6087179, lng: -79.5603365 }, "Dr. Cory Goldberg"],[{ lat: 43.6663211, lng: -79.3872946 }, "Toronto Eyelid Surgery"],[{ lat: 49.2126615, lng: -122.9223693 }, "Dr. Eric Pugash Aesthetic &amp; Plastic Surgery"],[{ lat: 43.7239064, lng: -79.3362581 }, "FORM Face &amp; Body"],[{ lat: 43.851577, lng: -79.4094398 }, "SKIN MED LASER CLINIC"],[{ lat: 43.851577, lng: -79.4094398 }, "SKIN MED LASER CLINIC"],[{ lat: 43.851577, lng: -79.4094398 }, "SKIN MED LASER CLINIC"],[{ lat: 43.7352464, lng: -79.4202923 }, "NU Youth Medispa"],[{ lat: 43.6496155, lng: -79.3846555 }, "Refine by MedCan"],[{ lat: 49.2637023, lng: -123.1250172 }, "Humphrey Cosmetic Dermatology"],[{ lat: 43.6757664, lng: -79.3967508 }, "Avenue MD Cosmetic Dermatology and Plastic Surgery"],[{ lat: 43.6757664, lng: -79.3967508 }, "Avenue MD Cosmetic Dermatology and Plastic Surgery"],[{ lat: 46.0926519, lng: -64.7808271 }, "Sparkle Lifestyle &amp; MediSpa"],[{ lat: 46.0926519, lng: -64.7808271 }, "Sparkle Lifestyle &amp; MediSpa"],[{ lat: 46.0926519, lng: -64.7808271 }, "Sparkle Lifestyle &amp; MediSpa"],[{ lat: 46.0926519, lng: -64.7808271 }, "Sparkle Lifestyle &amp; MediSpa"],[{ lat: 46.0926519, lng: -64.7808271 }, "Sparkle Lifestyle &amp; MediSpa"],[{ lat: 43.4472434, lng: -79.6668841 }, "Skinbetter Science "],[{ lat: 43.78972, lng: -79.4171678 }, "Viola Laser and Skin Care Clinic"],[{ lat: 49.6619727, lng: -112.796797 }, "Lethbridge Plastic Surgery"],[{ lat: 43.6858425, lng: -79.3774305 }, "Skinbosses Inc."],[{ lat: 43.7314129, lng: -79.4190645 }, "MC Beauty Cosmetic Clinic"],[{ lat: 49.0520595, lng: -122.3133858 }, "Valley Skin &amp; Laser Clinic-Abbotsford"],[{ lat: 49.1398244, lng: -121.9585623 }, "Valley Skin &amp; Laser Centre - Chilliwack"],[{ lat: 43.3941146, lng: -79.7092252 }, "Distinct Medical Aesthetics"],[{ lat: 50.4425806, lng: -104.5954508 }, "Head + Heart Skin Studio"],[{ lat: 43.7339005, lng: -79.4301479 }, "Dr. Romy Saibil"],[{ lat: 42.2988457, lng: -83.0266182 }, "Body &amp; Co. Medical Esthetics"],[{ lat: 43.3683696, lng: -80.9801786 }, "Centre Stage Medical Aesthetics"],[{ lat: 49.3185549, lng: -123.0728571 }, "North Shore Plastic Surgery"],[{ lat: 49.3185549, lng: -123.0728571 }, "North Shore Plastic Surgery"],[{ lat: 43.7327728, lng: -79.4197738 }, "DermAtelier on Avenue"],[{ lat: 43.4989138, lng: -79.8775897 }, "Fresh Faced Rx"],[{ lat: 54.4421591, lng: -124.2505427 }, "Fort St. James Medical Clinic"],[{ lat: 43.8156194, lng: -79.4264108 }, "Solomon Facial Plastics"],[{ lat: 43.7852427, lng: -79.2770563 }, "Holly Beauty Skin Clinic"],[{ lat: 43.6486362, lng: -79.3817439 }, "FCP Dermatology"],[{ lat: 43.8009975, lng: -79.4079432 }, "CBAM Clinic"],[{ lat: 50.428391, lng: -104.6309014 }, "INVU medical aesthetics "],[{ lat: 49.2630001, lng: -123.1212494 }, "Deez Skin Rej&#249;"],[{ lat: 42.9887882, lng: -81.2516897 }, "Skin Alchemy"],[{ lat: 42.9966828, lng: -81.248463 }, "Yazdani Aesthetics"],[{ lat: 42.3011982, lng: -82.9928248 }, "Healthy Image"],[{ lat: 43.8748224, lng: -79.2572945 }, "Brilliant Beauty &amp; Aesthetics"],[{ lat: 49.2798452, lng: -123.1246517 }, "Telus Health "],[{ lat: 43.9575474, lng: -78.9446794 }, "Canadian Beauty Boutique"],[{ lat: 49.2635975, lng: -123.1213494 }, "Dr. Peter Lennox"],[{ lat: 43.6511674, lng: -79.3789076 }, "reDivine Clinic Downtown"],[{ lat: 43.86436, lng: -79.383423 }, "reDivine Clinic"],[{ lat: 48.4550715, lng: -123.37544 }, "Studio Rococo"],[{ lat: 49.8576859, lng: -119.4897227 }, "Interior Dermatology Centre"],[{ lat: 43.2241196, lng: -79.7832219 }, "Glam Nurse Neda"],[{ lat: 49.3349026, lng: -123.1794947 }, "Dr. Shadan Kabiri Abyaneh"],[{ lat: 43.6604976, lng: -79.3860586 }, "Toronto Plastic Surgery"],[{ lat: 43.5826299, lng: -79.7238842 }, "Spa 6ix"],[{ lat: 49.2635975, lng: -123.1213494 }, "Vancouver Plastic Surgery"],[{ lat: 49.3498656, lng: -123.1639328 }, "Nexx MD"],[{ lat: 49.2922114, lng: -123.1337567 }, "Nancy Y. Van Laeken MD Inc."],[{ lat: 43.7231161, lng: -79.3759458 }, "Sunnybrook Dermatology "],[{ lat: 54.316711, lng: -130.3228591 }, "Aveline Mediclinic"],[{ lat: 44.2072609, lng: -79.4672683 }, "Ageless in Keswick"],[{ lat: 48.4324226, lng: -123.3235874 }, "Viso Dermatology"],[{ lat: 43.2009409, lng: -79.8949116 }, "DRS Skincare"],[{ lat: 49.2270231, lng: -122.9946155 }, "Metrotown Surgical Centre"],[{ lat: 49.2258454, lng: -122.8931799 }, "The Rai Clinic"],[{ lat: 43.9055128, lng: -80.1043175 }, "Jeremic Facial Cosmetics"],[{ lat: 49.2679266, lng: -123.15535 }, "AAM Anti-aging Medical &amp; Laser Clinic LTD"],[{ lat: 43.4453124, lng: -79.6683332 }, "The Aesthetic Haus"],[{ lat: 43.7644977, lng: -79.398598 }, "Versus Laser"],[{ lat: 49.8646965, lng: -97.1584994 }, "The A&#39;esthetic Skin Studio "],[{ lat: 43.5027522, lng: -80.1879299 }, "derma Skin Institute"],[{ lat: 46.4725737, lng: -81.0082289 }, "Parsons Dermatology"],[{ lat: 45.7791383, lng: -74.0137745 }, "Clinique MedicoDerma (DermaCosmetic)"],[{ lat: 43.4472434, lng: -79.6668841 }, "Skinbetter Science 官方医师"],[{ lat: 43.3308187, lng: -79.8060873 }, "Institute Of Facial Surgery"],[{ lat: 45.3448956, lng: -76.0300441 }, "Grace 2 Aesthetics"],[{ lat: 43.6245122, lng: -79.4852565 }, "Glam Nurse Jenny"],[{ lat: 43.8961972, lng: -78.8605362 }, "Omorphi Aesthetics"],[{ lat: 49.2673983, lng: -123.1188481 }, "FC Skin Solution"],[{ lat: 49.1120635, lng: -122.8093135 }, "Beauty Nook "],[{ lat: 43.7751271, lng: -79.6719836 }, "KIMKREATIONS"],[{ lat: 49.3263747, lng: -123.1351559 }, "Skinfolio"],[{ lat: 49.1697956, lng: -123.1481108 }, "Coastal Plastic Surgery"],[{ lat: 43.6743409, lng: -79.4007144 }, "Solomon Facial Plastic "],[{ lat: 47.0609735, lng: -55.1498867 }, "Touch &amp; Glow Aesthetics"],[{ lat: 45.4994, lng: -73.6481415 }, "Zina Derma Care Inc"],[{ lat: 49.2635975, lng: -123.1213494 }, "Dr. Sheina Macadam"],[{ lat: 53.54603, lng: -113.50135 }, "Sharmin Beauty Bar"],[{ lat: 43.7887313, lng: -79.5862754 }, "Woodbridge Dermatology Centre"],[{ lat: 48.6516411, lng: -123.5590252 }, "Renu Laser &amp; Skin Care Centre"],[{ lat: 49.3201309, lng: -123.0651113 }, "DERMLab"],[{ lat: 49.3201309, lng: -123.0651113 }, "DERMLab"],[{ lat: 49.3201309, lng: -123.0651113 }, "DERMLab"],[{ lat: 51.0298193, lng: -114.1349383 }, "Calgary MD Aesthetics"],[{ lat: 53.92845, lng: -122.84055 }, "Volare by Dr. Amy Johnson"],[{ lat: 49.1697956, lng: -123.1481108 }, "Coastal Plastic Surgery"],[{ lat: 49.1697956, lng: -123.1481108 }, "Coastal Plastic Surgery"],[{ lat: 44.4490717, lng: -64.3792271 }, "Cocoa Organic Spa &amp; Boutique"],[{ lat: 42.1866424, lng: -82.9113514 }, "Aurora Medical Aesthetics"],[{ lat: 53.2158619, lng: -114.9842457 }, "Chapman &amp; Dow Aesthetics"],[{ lat: 48.426763, lng: -123.328695 }, "Derma Spa"],[{ lat: 49.0310703, lng: -122.8671956 }, "Quartz Mind Body &amp; Skin Clinic Inc"],[{ lat: 49.506924, lng: -98.0018421 }, "Monarch Medical Aesthetics"],[{ lat: 49.2270231, lng: -122.9946155 }, "Metrotown Surgical Centre"],[{ lat: 43.6830873, lng: -79.3923227 }, "Clarity Medspa &amp; Laser Centre"],[{ lat: 44.3388144, lng: -79.6625339 }, "Barrie Women&#39;s Health Center"],[{ lat: 43.7160011, lng: -79.4547874 }, "Loft 7 Aesthetics"],[{ lat: 45.3347734, lng: -75.7232472 }, "Ottawa Derm Centre"],[{ lat: 55.1687294, lng: -118.7865518 }, "Studio Nade"],[{ lat: 49.2757027, lng: -123.1212254 }, "The Vanity Lab"],[{ lat: 43.6718544, lng: -79.3907576 }, "Miracle 10 - Yorkville"],[{ lat: 43.5637538, lng: -79.5927817 }, "Miracle 10 - The Plastic Surgery Clinic"],[{ lat: 49.2637957, lng: -123.1221784 }, "West Dermatology"],[{ lat: 49.3191074, lng: -123.0719987 }, "iSkinPure Clinic"],[{ lat: 42.9691781, lng: -81.3152757 }, "Jova Medical Aesthetics"],[{ lat: 49.3335662, lng: -123.0886187 }, "Afterglow Skin &amp; Laser Centre"],[{ lat: 48.1110782, lng: -123.1779168 }, "The SkinFix Studio Inc."],[{ lat: 49.6054067, lng: -97.0336118 }, "Ellvi Medspa"],[{ lat: 49.2683092, lng: -123.1490865 }, "Elena&#39;s Medi Spa"],[{ lat: 49.1253481, lng: -122.7062127 }, "Sensible Skin Lab"],[{ lat: 52.1489753, lng: -106.569038 }, "SKN Medical Spa"],[{ lat: 49.2751253, lng: -123.1328622 }, "One Clinic MD"],[{ lat: 43.2157096, lng: -79.7563288 }, "Aestheticare"],[{ lat: 49.6982032, lng: -123.1539412 }, "Lezara Laser &amp; Vein Care"],[{ lat: 43.4489278, lng: -79.6664147 }, "Revival Med Spa"],[{ lat: 43.5164734, lng: -79.6844393 }, "The Shore Skin Clinic"],[{ lat: 49.273145, lng: -123.1210885 }, "Clinic One Three Eight"],[{ lat: 49.273145, lng: -123.1210885 }, "Clinic One Three Eight"],[{ lat: 49.273145, lng: -123.1210885 }, "Clinic One Three Eight"],[{ lat: 49.273145, lng: -123.1210885 }, "Clinic One Three Eight"],[{ lat: 43.8649447, lng: -79.3128566 }, "BEAUTITUDE Medical Aesthetics"],[{ lat: 43.4773461, lng: -80.5256995 }, "Kraftw&#252;rk"],[{ lat: 45.3587533, lng: -75.9376788 }, "Kanata Plastic &amp; Cosmetic Surgery"],[{ lat: 49.32369, lng: -123.1033647 }, "Marine Drive Medical Clinic"],[{ lat: 49.8621928, lng: -119.4893007 }, "Omorfia Medical Aesthetics"],[{ lat: 43.241173, lng: -79.8534602 }, "Wyskt Facial Bar"],[{ lat: 49.7714171, lng: -97.3303836 }, "Reimer RNNP Services Corporation"],[{ lat: 45.8428636, lng: -64.1942873 }, "The Treatment Room"],[{ lat: 50.4421955, lng: -104.6112025 }, "Queen C Skin Studio"],[{ lat: 49.3201309, lng: -123.0651113 }, "DERMLab"],[{ lat: 43.4440579, lng: -80.5127391 }, "Subtly You"],[{ lat: 43.6899987, lng: -79.7650748 }, "Astra Medicare"],[{ lat: 49.471011, lng: -123.7582011 }, "NDK Med Spa"],[{ lat: 43.2234749, lng: -79.8652304 }, "Nfuzd Beauty + Healing Inc."],[{ lat: 42.9998553, lng: -81.3331514 }, "Dermacare By Despina"],[{ lat: 49.3111346, lng: -123.0804069 }, "Afterglow II Inc"],[{ lat: 43.888827, lng: -79.3158797 }, "Meraki Skin Clinic"],[{ lat: 49.2643619, lng: -123.1161528 }, "Pacific Plastic Surgery"],[{ lat: 49.2643619, lng: -123.1161528 }, "Pacific Plastic Surgery"],[{ lat: 49.3202347, lng: -123.0707104 }, "Dr. Alex Kuritzky"],[{ lat: 44.6394073, lng: -63.5889293 }, "Luma Clinic"],[{ lat: 44.6394073, lng: -63.5889293 }, "Luma Clinic"],[{ lat: 49.0406005, lng: -122.315272 }, "Abbotsford plastic Surgery"],[{ lat: 54.141089, lng: -115.6780853 }, "Radiate Laser and Esthetics"],[{ lat: 43.1446743, lng: -80.2739785 }, "Bare UR Beauty Medical Spa Inc"],[{ lat: 44.0694142, lng: -78.3774205 }, "Primrose Medical Aesthetics"],[{ lat: 55.2848533, lng: -114.7728254 }, "Skintasmic"],[{ lat: 44.2151853, lng: -77.3960506 }, "Forever Youth Aesthetics"],[{ lat: 0, lng: 0 }, ""]];
+
+
+  
+  let map;
+  
+  function initMap(data) {
+    map = new google.maps.Map(document.getElementById("map"), {
+          center: { lat:data.lat,lng:data.lng},
+      zoom: 8
+    });
+    const infoWindow = new google.maps.InfoWindow();
+    physicians.forEach(([position, title], i) => {
+      const marker = new google.maps.Marker({
+        position,
+        map,
+        title: `${title}`,
+        label: ``,
+        optimized: false,
+      });
+      // Add a click listener for each marker, and set up the info window.
+      marker.addListener("click", () => {
+        infoWindow.close();
+        infoWindow.setContent(marker.getTitle());
+        infoWindow.open(marker.getMap(), marker);
+        map.setCenter(marker.getPosition());
+        mapMoved();
+      });
+   });
+  
+    google.maps.event.addListener(map, 'dragend',function() {$('#autocomplete').val(''); mapMoved();}); 
+    initAutocomplete();
+    mapMoved();
+  
+  }
+  
+  function mapMoved(pLat,pLng) {
+    var mapcenter = map.getCenter();
+    $('#mapLat').val(mapcenter.lat());
+    $('#mapLng').val(mapcenter.lng());
+    //LoadPlaces($('#mapLat').val(),$('#mapLng').val());
+  }
+  
+
+
+  $(function() {
+  $.ajax({
+      url: 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyD7AjF9QrqugU_d9S0vimDXYhC6d7CqIeU',
+      data: JSON.stringify({ "considerIp": "true" }),
+      type: 'POST',
+      contentType: 'application/json',
+      error: function (request, error) {
+    georesult = JSON.parse('{"lat":43.4472434,"lng":-79.6668841}');
+    initMap(georesult);
+      },
+      success: function(data) {
+    georesult = JSON.parse('{"lat":' + data.location.lat + ',"lng":' + data.location.lng + '}');
+    //cis_createCookie('location',JSON.stringify({"lat":data.location.lat,"lng":data.location.lng}),3);
+    initMap(georesult);
+      }
+    });
+  });
+
+
+  
+  
+  var placeSearch, autocomplete;
+  
+  var componentForm = {
+    street_number: 'short_name',
+    route: 'long_name',
+    locality: 'long_name',
+    administrative_area_level_1: 'short_name',
+    country: 'long_name',
+    postal_code: 'short_name'
+  };
+  
+  function initAutocomplete() {
+    // Create the autocomplete object, restricting the search predictions to
+    // geographical location types.
+    autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'), {types: ['(regions)'], componentRestrictions: {country: "ca"}});
+  
+    // Avoid paying for data that you don't need by restricting the set of
+    // place fields that are returned to just the address components.
+    autocomplete.setFields(['address_component','geometry']);
+  
+    // When the user selects an address from the drop-down, populate the
+    // address fields in the form.
+    autocomplete.addListener('place_changed', function() {
+            console.log(autocomplete.getPlace());
+      var place = autocomplete.getPlace();
+        var data = {
+          lat: place.geometry.location.lat(),
+          lng: place.geometry.location.lng()
+        };
+      map.setCenter(data );
+      mapMoved();
+    }
+   );
+  
+    google.maps.event.addDomListener(document.getElementById('autocomplete'), 'keydown', function(event) { 
+      if (event.keyCode === 13) { 
+          event.preventDefault(); 
+      }
+    }); 
+  }
+  
+  // Bias the autocomplete object to the user's geographical location,
+  // as supplied by the browser's 'navigator.geolocation' object.
+  function geolocate() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var geolocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        var circle = new google.maps.Circle(
+            {center: geolocation, radius: position.coords.accuracy});
+        autocomplete.setBounds(circle.getBounds());
+      });
+    }
+  }
+
+
+  $(function() {
+    $('#autocomplete').val('');
+  });
+  
+  
